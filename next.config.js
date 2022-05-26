7713c49cf286864fa0530bd2470f929c89fc9f7f
Snowkids-code <oumaj47@gmail.com>
2022-05-26 05:46:46 +0300

    Sub-path routing and domain routing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  i18n: {
+    //all the locales to be supported in the app
+    locales: ["en-US", "fr", "nl-NL"],
+    //default locale when visiting a non-locale prefixed path - default lacale does not have a prefix
+    defaultLocale: "en-US",
+
+    domains: [
+      {
+        //subdomains must be included in the domain value to be matched
+        domain: 'example.com',
+        defaultLocale: 'en-US'
+      },
+      {
+        domain: 'example.fr',
+        defaultLocale: 'fr'
+      },
+      {
+        domain: 'example.nl',
+        defaultLocale: 'nl-NL',
+        //specify other locales that should be redirected to this domain
+        locales: ['nl-BE']
+      }
+    ]
+  },
+};
+
+
+//Using domain routing, you can configure loicales to be served from different domains
+
+// module.exports = {
+//   i18n: {
+//     locales:
+//   }
+// }
