179c5ce0b4a69022eb155f6de867555cdd2fd563
Snowkids-code <oumaj47@gmail.com>
2022-05-26 06:03:55 +0300

    Prefix the default locale

--- next.config.js
+++ next.config.js
@@ -5,38 +5,41 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
-module.exports = {
-  i18n: {
-    //all the locales to be supported in the app
-    locales: ["en-US", "fr", "nl-NL"],
-    //default locale when visiting a non-locale prefixed path - default lacale does not have a prefix
-    defaultLocale: "en-US",
+// module.exports = {
+//   i18n: {
+//     //all the locales to be supported in the app
+//     locales: ["en-US", "fr", "nl-NL"],
+//     //default locale when visiting a non-locale prefixed path - default lacale does not have a prefix
+//     defaultLocale: "en-US",
 
-    domains: [
-      {
-        //subdomains must be included in the domain value to be matched
-        domain: 'example.com',
-        defaultLocale: 'en-US'
-      },
-      {
-        domain: 'example.fr',
-        defaultLocale: 'fr'
-      },
-      {
-        domain: 'example.nl',
-        defaultLocale: 'nl-NL',
-        //specify other locales that should be redirected to this domain
-        locales: ['nl-BE']
-      }
-    ]
-  },
-};
+//     domains: [
+//       {
+//         //subdomains must be included in the domain value to be matched
+//         domain: 'example.com',
+//         defaultLocale: 'en-US'
+//       },
+//       {
+//         domain: 'example.fr',
+//         defaultLocale: 'fr'
+//       },
+//       {
+//         domain: 'example.nl',
+//         defaultLocale: 'nl-NL',
+//         //specify other locales that should be redirected to this domain
+//         locales: ['nl-BE']
+//       }
+//     ]
+//   },
+// };
 
 
 //Using domain routing, you can configure loicales to be served from different domains
 
-// module.exports = {
-//   i18n: {
-//     locales:
-//   }
-// }
+module.exports = {
+  i18n: {
+    locales: ['default', 'en', 'de', 'fr'],
+    defaultLocale: 'default',
+    localeDetection: false,
+  },
+  trailingSlash: true,
+}
